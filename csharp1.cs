fb4015ad3b0beb97d3bd2359166c76bf82e6effc
J4ckobs <93942652+J4ckobs@users.noreply.github.com>
2022-11-14 12:12:28 +0100

    Update csharp1.cs

--- csharp1.cs
+++ csharp1.cs
@@ -5,26 +5,34 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        //C.) sprawdza czy podane zmienne są sobie równe nie zależnie od wiellkości liter
+        //C.) sprawdza czy podane napisy są sobie równe nie zależnie od wiellkości liter
         static bool isEqual(string a, string b)
         {
             if (a.ToLower() == b.ToLower()) return true;
             else return false;
         }
         //E
-        static int ran(int a)
+        static int rand(int a)
         {
             Random r = new Random();
             return r.Next(a);
         }
         static string lotto(int amount, int range)
         {
+            /*
             string nums = "";
-            for (int i = 0; i < amount; i++) nums += " " + ran(range);
+            for (int i = 0; i < amount; i++) nums += " " + rand(range);
             return nums;
+            */
+            if (amount > 0)
+            {
+                return " " + rand(range) + lotto(amount-1,range);
+            }
+            return "";
+
         }
         //F
-        static double poleWalca(int r, int h)
+        static double poleWalca(int r, int h) //pole walca
         {
             if (h <= 0 || r <= 0)
                 return double.NaN;
@@ -32,7 +40,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 return 2 * (Math.PI * Math.Pow(r, 2)) + (2 * Math.PI * r * h); // 2* Pp + Pb = Pc
         }
 
-        static double vWalec(int r, int h)
+        static double vWalec(int r, int h) // objetosc walca
         {
             if (h <= 0 || r <= 0)
                 return double.NaN;
@@ -51,7 +59,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             return (b + sDelta) / (2 * a);
         }
         //I
-        static int sumSeven(int a)
+        static int sumSeven(int a) //suma liczb podzielnych przez 7
         {
             if (a == 0) return 0;
             else
@@ -63,11 +71,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
         static void Main(string[] args)
         {
-            Console.WriteLine(isEqual("ptaK", "PTAk")); // B.)
-            Console.WriteLine("Liczby losowanie lotto:"+lotto(5, 50)); // E.)
+            Console.WriteLine(isEqual("ptaK", "PTAk")); // B
+            Console.WriteLine("Liczby losowanie lotto:" + lotto(5, 50)); // E
             Console.WriteLine("Pole walca: wynosi: " + vWalec(5, 10) + "\nObjetosc walca wynosi: " + poleWalca(5, 10)); // F
-            Console.WriteLine("Jedno z miejsc zerowych f-cji: " + Solution(2, 6, 1)); // H.)
-            Console.WriteLine("Suma liczb podzielnych przez 7 dla zakresu (1 - 40): " + sumSeven(40)); //I.)
+            Console.WriteLine("Jedno z miejsc zerowych f-cji: " + Solution(2, 6, 1)); // H
+            Console.WriteLine("Suma liczb podzielnych przez 7 dla zakresu (1 - 40): " + sumSeven(40)); //I
         }
     }
 }
