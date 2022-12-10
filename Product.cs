de5fd2278886f4a57afc880a674f2a40f3dbff81
J4ckobs <93942652+J4ckobs@users.noreply.github.com>
2022-12-10 22:17:32 +0100

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB7
+{
+    internal class Product
+    {
+        private int currentPrice;
+        private string name;
+        private int mass;
+
+        public int CurrentPrice
+        {
+            get { return currentPrice; }
+            private set { currentPrice = WorldMarket.GetInitialPricePerKg(); }
+        }
+
+        public int Mass
+        {
+            get { return mass; }
+            set
+            {
+                if (value >= 0)
+                    mass = value;
+            }
+        }
+    }
+}
---
+++ WorldMarket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB7
+{
+    internal class WorldMarket
+    {
+        public static int GetInitialPricePerKg()
+        {
+            Random r = new Random();
+            Console.WriteLine(r.Next(100, 1001));
+            return r.Next(100,1001);
+        }
+
+        public static int GetNewPricePerKg(int a)
+        {
+            Random r = new Random();
+
+            return 0; ////////
+        
+    }
+}
